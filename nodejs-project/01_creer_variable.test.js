2efa70b9333920e8316a04a04ad76c624a94431d
Alain <alain@gallib.net>
2023-03-30 13:14:36 +0200

    Réorganisation

--- nodejs-project/01_creer_variable.test.js
+++ nodejs-project/01_creer_variable.test.js
@@ -1,17 +1,17 @@
-let utils = require('./utils.js');
-let assert = require('assert');
-var ex = require('./01_creer_variable.js');
+const utils = require('./utils.js');
+const assert = require('assert');
+const ex = require('./01_creer_variable.js');
 
 it("Création d'une variable'", function () {
-  try
-  {
-    assert.equal("Bonjour tout le monde !", ex.bonjour);
-    assert.equal(typeof(ex.bonjour), "string");
-  }
-  catch (error)
-  {
-    utils.printMessage('Conseil 💡', "Vérifiez le nom et/ou la valeur de la variable 🤔");
+    try
+    {
+        assert.equal("Bonjour tout le monde !", ex.bonjour);
+        assert.equal(typeof(ex.bonjour), "string");
+    }
+    catch (error)
+    {
+        utils.printMessage('Conseil 💡', "Vérifiez le nom et/ou la valeur de la variable 🤔");
 
-    throw error;
-  }
+        throw error;
+    }
 });
